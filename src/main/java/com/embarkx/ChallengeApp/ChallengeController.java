d9ed6691e0e89e41e9034bcfb66ec5343cf58d35
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-12-05 11:06:17 +0100

    Your commit message

---
+++ src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
@@ -0,0 +1,49 @@
+package com.embarkx.ChallengeApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ChallengeController {
+    private ChallengeService challengeService;
+
+    public ChallengeController(ChallengeService challengeService){
+        this.challengeService = challengeService;
+    }
+
+    @GetMapping("/challenges")
+    public ResponseEntity<List<Challenge>> getALlChallenges(){
+        return new ResponseEntity<>(challengeService.getAllChallenges(),HttpStatus.OK);
+    }
+
+    @PostMapping("/challenges")
+    public ResponseEntity<String> addChallenge(@RequestBody Challenge challenge){
+        boolean isChallengeAdded = challengeService.addChallenge(challenge);
+        if (isChallengeAdded)
+            return new ResponseEntity("Challenge added successfully",HttpStatus.OK);
+        else
+            return new ResponseEntity("Challenge  not added successfully",HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/challenges/{month}")
+    public ResponseEntity<Challenge> getChallenge(@PathVariable String month) {
+         Challenge challenge = challengeService.getChallenge(month);
+         if (challenge != null)
+             return new ResponseEntity<>(challenge, HttpStatus.OK);
+         else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/challenges/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id,@RequestBody Challenge updatedChallenge){
+        boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+        if (isChallengeUpdated)
+            return new ResponseEntity("Challenge updated successfully",HttpStatus.OK);
+        else
+            return new ResponseEntity("Challenge  not updated successfully",HttpStatus.NOT_FOUND);
+    }
+}
